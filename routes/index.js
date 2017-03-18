c59b3a31fdb6b8016c27a050bf17cbf601be6e27
jacscoder <et8fish@gmail.com>
2017-03-18 14:50:53 +0800

    feat(router): add upvote a comment

--- routes/index.js
+++ routes/index.js
@@ -54,7 +54,7 @@ router.put('/posts/:post/upvote', function(req, res, next) {
 });
 
 // creat a new comment for a post
-router.post('/posts/:id/comments', function(req, res, next) {
+router.post('/posts/:post/comments', function(req, res, next) {
   var comment = new Comment(req.body);
 
   comment.save(function(err, comment) {
@@ -63,9 +63,24 @@ router.post('/posts/:id/comments', function(req, res, next) {
   });
 });
 
-// upvote a comment
-router.put('/posts/:id/comments/:id/upvote', function(req, res, next) {
+router.param('comment', function(req, res, next, id) {
+  var query = Comment.findById(id);
 
+  query.exec(function(err, comment) {
+    if(err) { next(err); }
+    if(!comment) { return next(new Error('do not find comment')); }
+    req.comment = comment;
+    return next();
+  });
+});
+
+
+// upvote a comment
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+  req.comment.upvote(function(err, comment) {
+    if(err) { return next(err); }
+    res.json(comment);
+  });
 });
 
 module.exports = router;
